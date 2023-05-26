e3fb456d58512c8f83eacdf70414ce0a1d9e8414
GaboAGLE <59523483+GaboAGLE@users.noreply.github.com>
2023-05-26 05:59:03 +0000

    c

--- employees.py
+++ employees.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title('DSA05 Reto | Mi Plan de aplicación: Aplicación web de Ciencia de Datos')
 st.header('Desarrollado por: Gabriel Aguilar López Escalera')
@@ -86,3 +87,132 @@ st.write(empleados_filtrados)
 # Mostrar el total de empleados encontrados
 total_empleados_nivel = len(empleados_filtrados)
 st.write(f"Total de empleados encontrados: {total_empleados_nivel}")
+
+
+# Función para filtrar los empleados por ciudad
+def filtrar_por_ciudad(hometown):
+    employees = pd.read_csv(DATA_URL)
+    # Filtrar empleados según el nivel educativo seleccionado
+    resultado_ciudad = employees[employees['Hometown'] == hometown]
+    return resultado_ciudad
+
+# Diseño de la aplicación web con Streamlit
+
+st.sidebar.title("Filtro por Ciudad")
+
+# Obtener las ciudades únicos del DataFrame
+hometown = employees['Hometown'].unique()
+
+# Control selectbox en el sidebar para seleccionar el ciudad
+ciudad_seleccionado = st.sidebar.selectbox("Seleccionar ciudad", hometown)
+st.title("Buscador de Empleados por ciudad")
+# Filtrar los empleados por ciudad seleccionado
+empleados_ciudad = filtrar_por_ciudad(ciudad_seleccionado)
+
+# Mostrar el DataFrame con los resultados encontrados
+st.write("Resultados encontrados:")
+st.write(empleados_ciudad)
+
+# Mostrar el total de empleados encontrados
+total_empleados_ciudad = len(empleados_ciudad)
+st.write(f"Total de empleados encontrados: {total_empleados_ciudad}")
+
+
+
+# Función para filtrar los empleados por unit
+def filtrar_por_unit(unit):
+    employees = pd.read_csv(DATA_URL)
+    # Filtrar empleados según el nivel educativo seleccionado
+    resultado_unit = employees[employees['Unit'] == unit]
+    return resultado_unit
+
+# Diseño de la aplicación web con Streamlit
+
+st.sidebar.title("Filtro por Unit")
+
+# Obtener las ciudades únicos del DataFrame
+unit = employees['Unit'].unique()
+
+# Control selectbox en el sidebar para seleccionar unit
+unit_seleccionado = st.sidebar.selectbox("Seleccionar unit", unit)
+st.title("Buscador de Empleados por unit")
+# Filtrar los empleados por unit seleccionado
+empleados_unit = filtrar_por_unit(unit_seleccionado)
+
+# Mostrar el DataFrame con los resultados encontrados
+st.write("Resultados encontrados:")
+st.write(empleados_unit)
+
+# Mostrar el total de empleados encontrados
+total_empleados_unit = len(empleados_unit)
+st.write(f"Total de empleados encontrados: {total_empleados_unit}")
+
+
+# Crear el histograma
+fig, ax = plt.subplots()
+ax.hist(employees['Age'], bins=10)
+ax.set_xlabel('Edad')
+ax.set_ylabel('Número de Empleados')
+
+# Configurar la visualización con Streamlit
+st.title('Histograma de Edades de Empleados')
+st.pyplot(fig)
+
+
+# Calcular frecuencias de las unidades funcionales
+frecuencias = employees['Unit'].value_counts()
+
+# Crear gráfico de frecuencias
+fig, ax = plt.subplots()
+frecuencias.plot(kind='bar', ax=ax)
+
+# Configuración del gráfico
+ax.set_title('Frecuencias de Unidades Funcionales')
+ax.set_xlabel('Unidad Funcional')
+ax.set_ylabel('Número de Empleados')
+
+# Mostrar el gráfico en Streamlit
+st.title('Gráfica de frecuencias para las unidades funcionales')
+st.pyplot(fig)
+
+
+# Calcular el índice de deserción por ciudad
+employees_by_hometown = employees.groupby('Hometown').mean()
+desercion_por_ciudad = employees.groupby('Hometown')['Attrition_rate'].mean().sort_values(ascending=True)
+
+# Crear gráfico de barras horizontales
+fig, ax = plt.subplots()
+desercion_por_ciudad.plot(kind='bar', ax=ax)
+
+# Configuración del gráfico
+ax.set_title('Índice de Deserción por Ciudad')
+ax.set_xlabel('Ciudad')
+ax.set_ylabel('Índice de Deserción')
+
+# Mostrar el gráfico en Streamlit
+st.pyplot(fig)
+
+
+# Crear gráfico de dispersión
+
+fig, ax = plt.subplots()
+ax.scatter(employees['Age'], employees['Attrition_rate'], alpha=0.5)
+
+# Configuración del gráfico
+sns.scatterplot(y=employees['Attrition_rate'],x=employees['Age'])
+
+# Mostrar el gráfico en Streamlit
+st.pyplot(fig)
+
+
+# Crear gráfico de dispersión
+fig, ax = plt.subplots()
+ax.scatter(employees['Time_of_service'], employees['Attrition_rate'], alpha=0.5)
+
+# Configuración del gráfico
+ax.set_title('Tiempo de Servicio vs Tasa de Deserción')
+ax.set_xlabel('Tiempo de Servicio')
+ax.set_ylabel('Tasa de Deserción')
+
+# Mostrar el gráfico en Streamlit
+st.pyplot(fig)
